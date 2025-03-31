047cb564c4cd9f4b6076b046235d9b447ee08a58
gustab77 <75845025+gustab77@users.noreply.github.com>
2025-03-31 10:57:22 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,10 @@
+activate pytorch
+python main.py -m DSGRec -d baby -g 0
+
+
+activate pytorch
+ python main.py -m DSGRec -d sports -g 0
+
+
+ activate pytorch
+ python main.py -m DSGRec -d clothing -g 0
